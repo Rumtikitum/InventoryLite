6e9135c6b5317448f0519e44ce6ebd6e3e50c9d9
Richie Tauch <rtauc001@gmail.com>
2021-07-28 15:08:10 -0700

    logout notes

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -1,3 +1,4 @@
+//logout logic, If logged out it will start you at the login page
 async function logout() {
     const response = await fetch('/api/users/logout', {
       method: 'post',
@@ -5,10 +6,11 @@ async function logout() {
     });
   
     if (response.ok) {
+      //make sure to get the right route...this is routed to handlebars
       document.location.replace('/login');
     } else {
       alert(response.statusText);
     }
   }
-  
+  //query selector. Make sure id is correct
   document.querySelector('#logout').addEventListener('click', logout);
