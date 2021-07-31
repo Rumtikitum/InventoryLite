70e88f8708c539a760fa0a45339b93d89dfa189d
Richie Tauch <rtauc001@gmail.com>
2021-07-31 01:46:48 -0700

    almost fully done

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const {Item, Type} = require('../models/')
 const sequelize = require('../config/connection');
 //const { Post, User, Comment, Vote } = require('../models');
 
@@ -10,9 +11,23 @@ router.get('/', (req, res) => {
       });
     })
 
+router.get('/typetable', (req, res) => {
+  console.log('======================');
+    res.render('typetable', {
+        loggedIn: req.session.loggedIn
+      });
+    })
+
 router.get('/edit', (req, res) => {
   console.log('======================');
     res.render('edit', {
+        loggedIn: req.session.loggedIn
+      });
+    })
+
+router.get('/addtype', (req, res) => {
+  console.log('======================');
+    res.render('addtype', {
         loggedIn: req.session.loggedIn
       });
     })
@@ -31,6 +46,31 @@ router.get('/login', (req, res) => {
   }
 
   res.render('login');
+});
+
+router.get('/toc', async (req, res) => {
+  try {
+    // Get all items and JOIN with user data
+    const itemData = await Item.findAll({
+      include: [
+        {
+          model: Type,
+          attributes: ['type_name'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const items = itemData.map((item) => item.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('toc', { 
+      logged_in: req.session.logged_in,
+      items, 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
